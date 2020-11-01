42ab5659554866f60b8e890ee582a0032bb82a0c
munetsugu tamura <tsweblabo.2016@gmail.com>
2020-11-01 09:12:13 +0900

    update navbar
    navbarからのリンクにアニメーションを追加
    画面上部にnavbarを固定したことによるリンクの際の潜り込みを解消

--- JavaScript/navbar.js
+++ JavaScript/navbar.js
@@ -5,17 +5,5 @@ $(function(){
         // bodyにpadding-topをnavbarの高さで指定
         $('body').css('padding-top', height);
     });
-
-    // function pagelink(heightnum) {
-    //     var headerH = heightnum;
-    //     $("a.anchorlink").click(function(){
-    //         var href = $(this).attr("href");
-    //         var target = $(href == "#" || href == "" ? "body" : href);
-    //         var position = target.offset().top - headerH;
-    //         $("html, body").animate({ scrollTop: position }, 500, "swing");
-    //         //return false;
-    //     });
-    // }
-    // pagelink(height);
 });
 
